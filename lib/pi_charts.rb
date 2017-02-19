c3e1ad93bcb17462ff38b9dd0b3be38479c81fc0
Kent Gruber <kgruber1@emich.edu>
2017-02-19 10:12:04 -0500

    Comments, more comments.
    Need’em.

--- lib/pi_charts.rb
+++ lib/pi_charts.rb
@@ -1,3 +1,5 @@
+# I guess I could do a loop, jah feel?
+# Or any other way I guess.
 require "pi_charts/version"
 require "pi_charts/base"
 require "pi_charts/utils"
@@ -7,6 +9,7 @@ require "pi_charts/pie_chart"
 require "pi_charts/bar_chart"
 require "pi_charts/doughnut_chart"
 
+# Lil' extra spice.
 require "securerandom"
 require "json"
 
